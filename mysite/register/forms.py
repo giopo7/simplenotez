4df81bca421f259424f18f43d6df299912054d2a
giopo7 <politakisg@outlook.com>
2023-03-18 13:15:27 +0200

    fixed small buttons

--- mysite/register/forms.py
+++ mysite/register/forms.py
@@ -5,8 +5,8 @@ from django import forms
 
 
 class RegisterForm(UserCreationForm):
-	email = forms.EmailField()
+	# email = forms.EmailField()
 
 	class Meta:
 		model = User
-		fields = ["username", "email", "password1", "password2"]
+		fields = ["username", "password1", "password2"]
